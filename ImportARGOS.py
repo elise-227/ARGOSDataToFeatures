e3230f2b072b12fb5bec0ec2307ca0ae7d91d199
elise-227 <elise.boos@duke.edu>
2022-10-17 11:37:57 -0400

    created import file and added desc

---
+++ ImportARGOS.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 17 11:34:41 2022
+
+@author: eb371
+"""
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2022
+## Author: elise.boos@duke.edu (for ENV859)
+##---------------------------------------------------------------------
